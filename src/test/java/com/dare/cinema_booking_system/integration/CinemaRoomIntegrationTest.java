710e319d50f8135412ce95b8e2bde71b83c2c01f
Dare2510 <dario.boljevac@gmail.com>
2026-05-16 07:37:06 +0200

    implemented new IntegrationTest
    -updateRoom (when screening exits) returns bad request

--- src/test/java/com/dare/cinema_booking_system/integration/CinemaRoomIntegrationTest.java
+++ src/test/java/com/dare/cinema_booking_system/integration/CinemaRoomIntegrationTest.java
@@ -1,8 +1,13 @@
 package com.dare.cinema_booking_system.integration;
 
+import com.dare.cinema_booking_system.movies.dto.MovieRequest;
+import com.dare.cinema_booking_system.movies.entity.Genre;
 import com.dare.cinema_booking_system.rooms.dto.CinemaRoomRequest;
 import com.dare.cinema_booking_system.rooms.repository.CinemaRoomRepository;
 import com.dare.cinema_booking_system.rooms.repository.SeatRepository;
+import com.dare.cinema_booking_system.screenings.dto.ScreeningsRequest;
+import com.dare.cinema_booking_system.screenings.entity.TimeSlot;
+import com.dare.cinema_booking_system.screenings.repository.ScreeningsRepository;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +18,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -35,10 +43,14 @@ public class CinemaRoomIntegrationTest {
 	@Autowired
 	private SeatRepository seatRepository;
 
+	@Autowired
+	private ScreeningsRepository screeningsRepository;
+
 	@AfterEach
 	public void tearDown() {
 		cinemaRoomRepository.deleteAll();
 		seatRepository.deleteAll();
+		screeningsRepository.deleteAll();
 	}
 
 	@Test
@@ -80,7 +92,7 @@ public class CinemaRoomIntegrationTest {
 	}
 
 	@Test
-	public void createRoomAndUpdateRoom_whenJsonAndRoomNumberAreValid_returns200() throws Exception {
+	public void createRoomAndUpdateRoom_whenJsonAndRoomNumberAreValidAndNoScreeningExists_returns200() throws Exception {
 		CinemaRoomRequest request = new CinemaRoomRequest(1, 10, 20);
 
 		String responseJson = mockMvc.perform(post("/api/rooms")
@@ -104,7 +116,46 @@ public class CinemaRoomIntegrationTest {
 	}
 
 	@Test
-	public void createRoomAndDeleteRoom_whenRoomExists_returnsNoContent() throws Exception {
+	public void createRoomMovieScreeningUpdateRoom_whenJsonAndRoomNumberAreValidAndScreeningExists_returnsBadRequest() throws Exception {
+		CinemaRoomRequest request = new CinemaRoomRequest(1, 10, 20);
+
+		String roomResponseJson = mockMvc.perform(post("/api/rooms")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isCreated())
+				.andReturn().getResponse().getContentAsString();
+
+		Long roomId = ((Number) JsonPath.read(roomResponseJson, "$.id")).longValue();
+
+		MovieRequest movieRequest = new MovieRequest("TestTitle", "TestDescription", 90, Genre.FANTASY);
+
+		String movieResponseJson = mockMvc.perform(post("/api/movies")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(movieRequest)))
+				.andReturn().getResponse().getContentAsString();
+
+		Long movieId = ((Number) JsonPath.read(movieResponseJson, "$.id")).longValue();
+
+		ScreeningsRequest screeningsRequest = new ScreeningsRequest(roomId, movieId, LocalDate.now(), TimeSlot.PRIME, BigDecimal.valueOf(5));
+
+		mockMvc.perform(post("/api/screening")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(screeningsRequest)))
+				.andExpect(status().isOk());
+
+		request.setRowCapacity(25);
+
+		mockMvc.perform(patch("/api/rooms/update/" + roomId)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.message")
+						.value("Changes are not possible for cinema room with id " + roomId + " screening exits"));
+
+	}
+
+	@Test
+	public void createRoomAndDeleteRoom_whenRoomExistsButNoScreeningExists_returnsNoContent() throws Exception {
 		CinemaRoomRequest request = new CinemaRoomRequest(1, 10, 20);
 
 		String responseJson = mockMvc.perform(post("/api/rooms")
